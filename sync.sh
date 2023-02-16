1601e4aad77552efcf1edb5b1788ba3b58f42010
serialt <t@imau.cc>
2023-02-16 23:17:00 +0800

    feat: 迷你版

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hub="docker.io"
-repo="$hub/serialt"
+# hub="docker.io"
+# repo="$hub/serialt"
 
 hub2="registry.cn-hangzhou.aliyuncs.com"
 repo2="$hub2/serialt"
@@ -11,10 +11,10 @@ repo2="$hub2/serialt"
 if [ -f sync.yaml ]; then
    echo "[Start] sync......."
    
-    sudo skopeo login -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub} \
-    && sudo skopeo --insecure-policy sync -a --src yaml --dest docker sync.yaml ${repo} \
-    && sudo skopeo --insecure-policy sync -a --src yaml --dest docker custom_sync.yaml ${repo}
-    sleep 3
+    # sudo skopeo login -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub} \
+    # && sudo skopeo --insecure-policy sync -a --src yaml --dest docker sync.yaml ${repo} \
+    # && sudo skopeo --insecure-policy sync -a --src yaml --dest docker custom_sync.yaml ${repo}
+    # sleep 3
     sudo skopeo login -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub2} \
     && sudo skopeo --insecure-policy sync -a --src yaml --dest docker sync.yaml ${repo2} \
     && sudo skopeo --insecure-policy sync -a  --src yaml --dest docker custom_sync.yaml ${repo2}
